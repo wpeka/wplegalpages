07598b1b558a30c483edbe8fe94bb02fa7235e42
Prajakti Sahay <prajakti.sahay@hbwsl.com>
2018-03-09 16:35:27 +0530

    steps to upgrade added

--- SurveyFunnelWithAddon/SurveyFunnelWithAddon/surveyfunnel/surveyfunnel/includes/classes/survey_classes/all_classes.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include all survey related classes
-include_once(dirname(__FILE__) . "/survey_activity_class.php");
-include_once(dirname(__FILE__) . "/survey_class.php");
-include_once(dirname(__FILE__) . "/survey_export_class.php");
-include_once(dirname(__FILE__) . "/survey_manage_class.php");
-include_once(dirname(__FILE__) . "/survey_question_manage_class.php");
-?>
