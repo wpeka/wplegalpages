8b8d699e4d04b46da58b9c0551876842d933ca03
dhananjay <dhananjay@hbwsls-MacBook-Air.local>
2019-12-18 14:50:59 +0530

    Updated link for show credit, made compatible with WordPress 5.3.1, Updated readme.

--- includes/class-wp-legal-pages.php
+++ includes/class-wp-legal-pages.php
@@ -182,6 +182,10 @@ if ( ! class_exists( 'WP_Legal_Pages' ) ) {
 		 */
 		private function define_public_hooks() {
 			$plugin_public = new WP_Legal_Pages_Public( $this->get_plugin_name(), $this->get_version() );
+			$lp_general    = get_option( 'lp_general' );
+			if ( isset( $lp_general['generate'] ) && '1' === $lp_general['generate'] ) {
+				$this->loader->add_filter( 'the_content', $plugin_public, 'wplegal_post_generate' );
+			}
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_script' ) );
 			add_action( 'wp_footer', array( $this, 'wp_legalpages_show_eu_cookie_message' ) );
 
