eb4e7fd7dd0faaf3e2f81eff3f07ff7cdec2eedc
Ujjawal <ujjawal.chhetri@hbwsl.com>
2024-05-07 11:22:37 +0530

    add changes

--- wplegalpages.php
+++ wplegalpages.php
@@ -34,7 +34,7 @@ if ( ! defined( 'WPLEGAL_API_ADMIN_URL' ) ) {
  * Check if the constant GDPR_APP_URL is not already defined.
 */
 if ( ! defined( 'WPLEGAL_APP_URL' ) ) {
-	define( 'WPLEGAL_APP_URL', 'https://d8c7243ac1.nxcli.io' );
+	define( 'WPLEGAL_APP_URL', 'https://app.wplegalpages.com' );
 }
 
 if ( ! function_exists( 'wplp_fs' ) ) {
