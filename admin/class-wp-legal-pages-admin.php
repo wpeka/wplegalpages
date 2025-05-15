30ef4fa7827046764e03830d97840d262903a2bf
Aman Semwal <aman.semwal@hbwsl.com>
2025-05-15 09:52:14 +0530

    Version no and changelog.

--- admin/class-wp-legal-pages-admin.php
+++ admin/class-wp-legal-pages-admin.php
@@ -427,7 +427,7 @@ if ( ! class_exists( 'WP_Legal_Pages_Admin' ) ) {
 			$current_version = $this->version;
 		
 			// Target version to hide the submenu
-			$target_version = '3.3.7';
+			$target_version = '3.3.8';
 		
 			// Check if the current version is below the target version
 			if (version_compare($current_version, $target_version, '<')) {
