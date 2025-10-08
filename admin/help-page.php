79c44b0b18ce3b502eb05ff174912aae9160626f
DAnn2012 <dannunziata@hotmail.it>
2025-10-08 10:32:11 +0200

    Update help-page.php

--- admin/help-page.php
+++ admin/help-page.php
@@ -41,7 +41,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                         </div>
                         <h3><?php esc_html_e( 'Documentation', 'wplegalpages' ); ?></h3>
                         <p><?php esc_html_e( 'Browse our step-by-step guides and articles to help you get started and troubleshoot with ease.', 'wplegalpages' ); ?></p>
-                        <a href="<?php echo esc_url( 'https://wplegalpages.com/docs/wp-cookie-consent/', 'wplegalpages' ); ?>" target="_blank"><?php esc_html_e( "Read Documentation." ); ?><img class="gdpr-other-plugin-arrow" src="<?php echo esc_url( WPL_LITE_PLUGIN_URL . 'admin/js/vue/images/blue_right_arrow.svg' ); ?>"></a>
+                        <a href="<?php echo esc_url( 'https://wplegalpages.com/docs/wp-cookie-consent/', 'wplegalpages' ); ?>" target="_blank"><?php esc_html_e( "Read Documentation.", 'wplegalpages' ); ?><img class="gdpr-other-plugin-arrow" src="<?php echo esc_url( WPL_LITE_PLUGIN_URL . 'admin/js/vue/images/blue_right_arrow.svg' ); ?>"></a>
                       </div>
 
                       <div class="wplegalpages-help-card">
@@ -52,7 +52,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                         </div>
                         <h3><?php esc_html_e( 'Video Tutorials', 'wplegalpages' ); ?></h3>
                         <p><?php esc_html_e( 'Prefer learning by watching? Explore our tutorials to see the plugin in action and learn how to use it effectively.', 'wplegalpages' ); ?></p>
-                        <a href="<?php echo esc_url( 'https://wplegalpages.com/docs/wp-cookie-consent/video-guides/video-resources/','wplegalpages' ); ?>" target="_blank"><?php esc_html_e( "Watch Now." ); ?><img class="gdpr-other-plugin-arrow" src="<?php echo esc_url( WPL_LITE_PLUGIN_URL . 'admin/js/vue/images/blue_right_arrow.svg' ); ?>"></a>
+                        <a href="<?php echo esc_url( 'https://wplegalpages.com/docs/wp-cookie-consent/video-guides/video-resources/','wplegalpages' ); ?>" target="_blank"><?php esc_html_e( "Watch Now.", 'wplegalpages' ); ?><img class="gdpr-other-plugin-arrow" src="<?php echo esc_url( WPL_LITE_PLUGIN_URL . 'admin/js/vue/images/blue_right_arrow.svg' ); ?>"></a>
                       </div>
 
                       <div class="wplegalpages-help-card">
@@ -63,7 +63,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                         </div>
                         <h3><?php esc_html_e( 'Request a Feature', 'wplegalpages' ); ?></h3>
                         <p><?php esc_html_e( 'Got an idea that could make the plugin better? We’d love to hear from you.', 'wplegalpages' ); ?></p>
-                        <a href="<?php echo esc_url( 'https://wplegalpages.com/contact-us/', 'wplegalpages' ); ?>" target="_blank"><?php esc_html_e( "Request Now." ); ?><img class="gdpr-other-plugin-arrow" src="<?php echo esc_url( WPL_LITE_PLUGIN_URL . 'admin/js/vue/images/blue_right_arrow.svg' ); ?>"></a>
+                        <a href="<?php echo esc_url( 'https://wplegalpages.com/contact-us/', 'wplegalpages' ); ?>" target="_blank"><?php esc_html_e( "Request Now.", 'wplegalpages' ); ?><img class="gdpr-other-plugin-arrow" src="<?php echo esc_url( WPL_LITE_PLUGIN_URL . 'admin/js/vue/images/blue_right_arrow.svg' ); ?>"></a>
                       </div>
                     </section>
                     
